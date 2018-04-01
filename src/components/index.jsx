6d3e112fa58b4004b1aa10e0a467c23d4d46599a
lorozco <liss612@gmail.com>
2018-04-01 18:41:47 -0500

    u

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,21 +1,38 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 // class App extends React.Component {
 class App extends Component {
   constructor(...props) { // spread operator
     super(...props);
 
-    this.state = {}
+    this.state = {
+      courses: [
+        {id: 1, name: 'React desde Cero', teacher: 'Jonathan Mircha'},
+        {id: 2, name: 'Drupal 8 desde cero', teacher: 'Alberto Quiroga'}
+      ]
+    }
   }
 
   render() { // sólo debe retornar un nodo hijo
     return(
-      <div>Hola React</div>
+      <div>
+        <form>
+          <input type="text" placeholder="Nombre del Curso" name="name" id="" required/>
+          <input type="text" placeholder="Profesor" name="teacher" id="" required/>
+          <input type="hidden" name="" id="" value={Math.floor(Math.random()*100)}/>
+          <input type="submit" value="Guardar"/>
+        </form>
+        <ul>
+          <li>Curso 1</li>
+          <li>Curso 2</li>
+        </ul>
+      </div>
     ) 
   }
 }
 
-App.protoTypes = {}
+App.propTypes = {}
 App.defaultProps = {}
 
 export default App
